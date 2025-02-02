df5252ffcadbfc2bfdb1de630c1efdd588afdacd
hatemfaysil16 <hatemfaysal15@gmail.com>
2025-02-02 17:02:00 +0200

    dd

--- Components/TutorialMenu/Default.cs
+++ Components/TutorialMenu/Default.cs
@@ -4,9 +4,14 @@ namespace testcrud.Components.TutorialMenu
 {
     public class TutorialMenuViewComponent:ViewComponent
     {
-        public  IViewComponentResult Invoke(string category)
+        public  IViewComponentResult Invoke(string category,string view,string controller,string action,string title,string editValue)
         {
             ViewData["category"] = category;
+            ViewData["view"] = view;
+            ViewData["controller"] = controller;
+            ViewData["action"] = action;
+            ViewData["title"] = title;
+            ViewData["editValue"] = editValue;
             return View();
         }
     }
--- obj/Debug/net9.0/en-US/testcrud.resources.cs
+++ obj/Debug/net9.0/en-US/testcrud.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testcrud")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6497031e44052e3270030fc694bff5a5059bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f3b1eb9de349d2c5dc5016cc6721bf91e21c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("testcrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testcrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
